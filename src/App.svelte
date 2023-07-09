4bd2c0de32d27a4521ef0a701cb7cbfbd4eeb245
jiyoung-B <catgirl0313@naver.com>
2023-07-09 15:21:31 +0900

    유저 인증까지 파이어베이스로 구현

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,61 @@
 <script>
   import Login from "./pages/Login.svelte";
+  import Loading from "./pages/Loading.svelte";
   import Main from "./pages/Main.svelte";
   import NotFound from "./pages/NotFound.svelte";
   import Signup from "./pages/Signup.svelte";
   import Write from "./pages/Write.svelte";
   import Router from "svelte-spa-router";
   import "./css/style.css";
+  import { user$ } from "./store";
+  import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount } from "svelte";
+  import MyPage from "./pages/MyPage.svelte";
+  // import { GoogleAuthProvider } from "firebase/auth";
+
+  // const provider = new GoogleAuthProvider();
+  // provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+
+  // let login = false;
+
+  let isLoading = true;
+
+  const auth = getAuth();
+
+  const checkLogin = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   const routes = {
     "/": Main,
-    "/login": Login,
+    // "/login": Login,
     "/signup": Signup,
     "/write": Write,
+    "/my": MyPage,
     "*": NotFound,
   };
+
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
 
 <main>
   <div>안녕하세요</div>
--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,15 +1,53 @@
-<form ID="login-form" action="/signup" method="POST">
-  <div>로그인 하기</div>
-  <div>
-    <label for="id">아이디</label>
-    <input type="text" id="id" name="id" required />
-  </div>
-  <div>
-    <label for="password">패스워드</label>
-    <input type="password" id="password" name="password" required />
-  </div>
-  <div>
-    <button type="submit">로그인</button>
-  </div>
-  <div id="info" />
-</form>
+<script>
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { user$ } from "../store";
+
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      user$.set(user);
+      // console.log(token, user);
+
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+</script>
+
+<div>
+  <!-- {#if $user$}
+    <div>{$user$?.displayName}</div>
+  {/if} -->
+  <div>로그인하기</div>
+  <button class="login-btn" on:click={loginWithGoogle}>
+    <img
+      class="google-img"
+      src="https://w7.pngwing.com/pngs/326/85/png-transparent-google-logo-google-text-trademark-logo-thumbnail.png"
+      alt=""
+    />
+    <div>Google로 시작하기</div>
+  </button>
+</div>
+
+<style>
+  .login-btn {
+    width: 200px;
+    height: 50px;
+    border: 1px solid gray;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    border-radius: 3px;
+  }
+  .google-img {
+    width: 20px;
+  }
+</style>
